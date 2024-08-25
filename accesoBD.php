670c6de27f15d7366ce4cb65e6b8faab88f3e23e
telemetrial <leyver.mosqueram@comunidad.iush.edu.co>
2024-08-25 10:52:17 -0500

    Update accesoBD.php

--- accesoBD.php
+++ accesoBD.php
@@ -18,9 +18,9 @@ class accesoBD{
     return $pArray;
   }
   public static function subirDatos($valor, $tipo){
-  $pArray = new stdClass();
-  $fecha = date ('Y-m-d H:i:s');
-  $sql= "INSERT INTO datos (type, value, date) VALUES ('{$tipo}','{$valor}','{$fecha}')";
+    $pArray = new stdClass();
+    $fecha = date ('Y-m-d H:i:s');
+    $sql= "INSERT INTO datos (type, value, date) VALUES ('{$tipo}','{$valor}','{$fecha}')";
   if (conexion::ejecuctarSQL($sql)) $pArray->code = '200';
   else $pArray->code = '300';
   return $pArray;
